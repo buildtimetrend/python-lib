70546453cc1bdef4c1a7868f728008b8d08b94b1
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-03-08 18:27:58 +0100

    don't display end timestamp on STDOUT

--- analyse.sh
+++ analyse.sh
@@ -19,7 +19,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 if [ "$BUILD_TREND_INIT" == "1" ]; then
-    timestamp.sh end
+    timestamp.sh -q end
     analyse.py
 else
     echo "Build-trend not initialised, run 'source init.sh'"
