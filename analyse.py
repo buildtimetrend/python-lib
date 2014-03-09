329895cd74065c2daeb9ea819ef0b2d6352dd9ed
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-03-09 15:01:38 +0100

    use tabs for indentation

--- analyse.py
+++ analyse.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# vim: set noexpandtab sw=4 ts=4:
 import csv
 import os
 # use parameter for timestamps file and check if file exists
@@ -6,14 +7,14 @@ timeStampFile = os.getenv('BUILD_TREND_LOGFILE', 'timestamps.csv')
 if not os.path.isfile(timeStampFile):quit()
 
 with open(timeStampFile, 'rb') as csvfile:
-    timestamps = csv.reader(csvfile, delimiter=',', quotechar='"')
-    previousTime = 0
-    eventName = None
-    for row in timestamps:
-	if eventName is not None:
-	    if eventName == 'end':
-                break
-	    duration = int(row[1]) - previousTime
-	    print 'Duration ' + eventName + ' : ' + str(duration) + 's'
-	eventName = row[0]
-	previousTime = int(row[1])
+	timestamps = csv.reader(csvfile, delimiter=',', quotechar='"')
+	previousTime = 0
+	eventName = None
+	for row in timestamps:
+		if eventName is not None:
+			if eventName == 'end':
+				break
+			duration = int(row[1]) - previousTime
+			print 'Duration ' + eventName + ' : ' + str(duration) + 's'
+		eventName = row[0]
+		previousTime = int(row[1])
