491368f18aeaea67a2b1a4e0cd85bd2d141460e4
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-04-10 22:08:07 +0200

    use python 3 format for formatting string

--- generate_trend.py
+++ generate_trend.py
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     trend = Trend()
     if trend.gather_data(RESULT_FILE):
         # print number of builds and list of buildnames
-        print('Builds (%d) :' % len(trend.builds), trend.builds)
-        print('Stages (%d) :' % len(trend.stages), trend.stages)
+        print 'Builds ({}) :'.format(len(trend.builds)), trend.builds
+        print 'Stages ({}) :'.format(len(trend.stages)), trend.stages
         trend.generate(GRAPH_FILE)
