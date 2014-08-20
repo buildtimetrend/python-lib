f58f4ef0ef3e49d93acf7a6540f35909bef7c2f1
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-08-20 21:27:15 +0200

    improve module comment

--- get_read_key.py
+++ get_read_key.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 # vim: set expandtab sw=4 ts=4:
-# Generate a read key for Keen.io trends
-# Usage : get_read_key.py project_name
-#
-# Copyright (C) 2014 Dieter Adriaenssens <ruleant@users.sourceforge.net>
-#
-# This file is part of buildtime-trend
-# <https://github.com/ruleant/buildtime-trend/>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+Generate a read key for Keen.io API
+
+Usage : get_read_key.py [project_name]
+
+When argument project_name is used, the read key will be
+generated to access this project.
+If no argument is given, the default project name will be used.
+
+Copyright (C) 2014 Dieter Adriaenssens <ruleant@users.sourceforge.net>
+
+This file is part of buildtime-trend
+<https://github.com/ruleant/buildtime-trend/>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
 
 from buildtimetrend.keenio import keen_io_generate_read_key
 import sys
