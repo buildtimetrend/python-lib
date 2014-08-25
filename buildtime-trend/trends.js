edba198f44558c20c2a5801e3b6945ce8ce4c070
travis-ci <travis@travis-ci.org>
2014-08-25 16:16:55 +0000

    buildtime-trend of build 259.1 synchronised with gh-pages

--- buildtime-trend/trends.js
+++ buildtime-trend/trends.js
@@ -307,13 +307,32 @@ function initCharts() {
 
     // draw chart
     var requestAvgBuildtimeHour = client.run(queryAvgBuildtimeHour, function() {
-      this.draw(document.getElementById("chart_avg_buildtime_hour"), {
-        chartType: "columnchart",
+      chart_data = []
+      // populate array with an entry per hour
+      for (i = 0; i < 24; i++) {
+        chart_data[i]={
+          caption: i + ":00",
+          result: 0
+        }
+      }
+      // copy query data into the populated array
+      for (i=0; i < this.data.result.length; i++) {
+        chart_data[this.data.result[i]["build.started_at.hour_24"]]["result"] = this.data.result[i]["result"];
+      }
+
+      window.chart = new Keen.Visualization(
+	{result: chart_data},
+	document.getElementById("chart_avg_buildtime_hour"),
+	{chartType: "columnchart",
         title: "Average buildtime per time of day",
         chartOptions: {
           legend: { position: "none" },
           vAxis: { title: "duration [s]" },
-          hAxis: { title: "Time of day [24-hour format, UTC]" }
+          hAxis: {
+		    title: "Time of day [24-hour format, UTC]",
+		    slantedText: "true",
+			slantedTextAngle: "90"
+		  }
         }
       });
     });
