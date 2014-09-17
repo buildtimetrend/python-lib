f1f595723c68bcf5461818b3c268c9e94673a073
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-09-17 20:10:02 +0200

    add file comment and license

--- trends/trends.js
+++ trends/trends.js
@@ -1,3 +1,26 @@
+/* vim: set expandtab sw=4 ts=4: */
+/**
+ * Gathers build related data.
+ *
+ * Copyright (C) 2014 Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ *
+ * This file is part of buildtime-trend
+ * <https://github.com/ruleant/buildtime-trend/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var TIMEFRAME_LAST_WEEK = "this_7_days";
 var TIMEFRAME_LAST_MONTH = "this_30_days";
 var TIMEFRAME_LAST_YEAR = "this_52_weeks";
