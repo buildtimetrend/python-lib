057fb55e43754203a22f0c9dd469208572911d36
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-09-30 18:13:30 +0200

    add method to set loglevel, issue #49

--- buildtimetrend/__init__.py
+++ buildtimetrend/__init__.py
@@ -23,7 +23,23 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
+import logging
 
 VERSION = "0.2-dev"
 SCHEMA_VERSION = "2"
 USER_AGENT = "buildtime-trend/" + VERSION
+
+
+def set_loglevel(loglevel):
+    '''
+    Sets loglevel
+    Based on example on https://docs.python.org/2/howto/logging.html
+
+    Assuming loglevel is bound to the string value obtained from the
+    command line argument. Convert to upper case to allow the user to
+    specify --log=DEBUG or --log=debug
+    '''
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    logging.basicConfig(level=numeric_level)
