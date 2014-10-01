f0dfb4e30fe5b2e078e6d1cbbec16a5f05dba315
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-10-01 17:27:32 +0200

    set loglevel with setLevel

--- buildtimetrend/__init__.py
+++ buildtimetrend/__init__.py
@@ -42,4 +42,6 @@ def set_loglevel(loglevel):
     numeric_level = getattr(logging, loglevel.upper(), None)
     if not isinstance(numeric_level, int):
         raise ValueError('Invalid log level: %s' % loglevel)
-    logging.basicConfig(level=numeric_level)
+    #logging.basicConfig(level=numeric_level)
+    logger = logging.getLogger()
+    logger.setLevel(numeric_level)
