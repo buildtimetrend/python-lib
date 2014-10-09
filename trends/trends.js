a6200a262fa27ac91314b6ebf0d70e1d64ab9c80
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-10-09 21:29:32 +0200

    improve file description [skip ci]

--- trends/trends.js
+++ trends/trends.js
@@ -1,6 +1,6 @@
 /* vim: set expandtab sw=4 ts=4: */
 /**
- * Gathers build related data.
+ * Analyse and visualise trend data using the Keen.io API.
  *
  * Copyright (C) 2014 Dieter Adriaenssens <ruleant@users.sourceforge.net>
  *
