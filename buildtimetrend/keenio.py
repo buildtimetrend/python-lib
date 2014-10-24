bb24c8a750680501991e568242b859cd6e7bce33
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-10-24 16:21:42 +0200

    check write and read key using keen instance

--- buildtimetrend/keenio.py
+++ buildtimetrend/keenio.py
@@ -36,7 +36,8 @@ def keen_io_writable():
     '''
     Check if login keys for Keen IO API are set, to allow writing.
     '''
-    if "KEEN_PROJECT_ID" in os.environ and "KEEN_WRITE_KEY" in os.environ:
+    if ("KEEN_PROJECT_ID" in os.environ and "KEEN_WRITE_KEY" in os.environ or
+        keen.project_id is not None and keen.write_key is not None):
         return True
     return False
 
@@ -45,7 +46,8 @@ def keen_io_readable():
     '''
     Check if login keys for Keen IO API are set, to allow reading.
     '''
-    if "KEEN_PROJECT_ID" in os.environ and "KEEN_READ_KEY" in os.environ:
+    if ("KEEN_PROJECT_ID" in os.environ and "KEEN_READ_KEY" in os.environ or
+        keen.project_id is not None and keen.read_key is not None):
         return True
     return False
 
