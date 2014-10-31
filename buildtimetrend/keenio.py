22e0a5e5fcb762a8eeb612823e99d43403e30165
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-10-31 02:13:38 +0100

    fix indentation

--- buildtimetrend/keenio.py
+++ buildtimetrend/keenio.py
@@ -37,7 +37,7 @@ def keen_io_writable():
     Check if login keys for Keen IO API are set, to allow writing.
     '''
     if ("KEEN_PROJECT_ID" in os.environ and "KEEN_WRITE_KEY" in os.environ or
-        keen.project_id is not None and keen.write_key is not None):
+            keen.project_id is not None and keen.write_key is not None):
         return True
 
     logging.warning("Keen.io Write Key is not set")
@@ -49,7 +49,7 @@ def keen_io_readable():
     Check if login keys for Keen IO API are set, to allow reading.
     '''
     if ("KEEN_PROJECT_ID" in os.environ and "KEEN_READ_KEY" in os.environ or
-        keen.project_id is not None and keen.read_key is not None):
+            keen.project_id is not None and keen.read_key is not None):
         return True
 
     logging.warning("Keen.io Read Key is not set")
