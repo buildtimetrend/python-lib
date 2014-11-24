7690467bb57327f86ecf7f793ef68d022bd77b39
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-11-24 13:03:46 +0100

    add classifiers [skip ci]

--- setup.py
+++ setup.py
@@ -18,7 +18,17 @@ setup(
     author="Dieter Adriaenssens",
     author_email="ruleant@users.sourceforge.net",
     description="Visualise what's trending in your build process",
-    license="GPLv3",
+    url="http://ruleant.github.io/buildtime-trend/",
+    license="GPLv3+",
     keywords=["trends", "charts", "build", "ci", "timing data"],
-    url="http://ruleant.github.io/buildtime-trend/"
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance"
+    ]
 )
