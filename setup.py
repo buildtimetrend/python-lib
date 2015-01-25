86116f383549cb2291e370386db5c5f474245d17
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-01-25 17:45:54 +0100

    increment build number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="buildtimetrend",
-    version="0.2.dev21",
+    version="0.2.dev22",
     packages=find_packages(),
     install_requires=['keen', 'lxml', 'pyyaml', 'python-dateutil'],
     tests_require=['nose', 'coveralls'],
