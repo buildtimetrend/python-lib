40b73ebeee78bc89c3dec434977f8a77e511efaf
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-02-16 20:01:11 +0100

    start preparing v0.3

--- buildtimetrend/__init__.py
+++ buildtimetrend/__init__.py
@@ -29,7 +29,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 from buildtimetrend.tools import set_loglevel
 
 NAME = "buildtimetrend"
-VERSION = "0.2"
+VERSION = "0.3.dev"
 SCHEMA_VERSION = "2"
 USER_AGENT = "%s/%s" % (NAME, VERSION)
 
