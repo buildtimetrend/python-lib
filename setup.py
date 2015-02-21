4cac13c91e6260e2b37b8ff69f891d2144744f9d
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-02-21 17:27:47 +0100

    add long description to setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,26 @@ setup(
     author="Dieter Adriaenssens",
     author_email="ruleant@users.sourceforge.net",
     description="Visualise what's trending in your build process",
+    long_description="Buildtime Trend generates and gathers timing data of " \
+    "build processes. The aggregated data is used to create charts to " \
+    "visualise trends of the build process.\n" \
+    "These trends can help you gain insight in your build process : " \
+    "which stages take most time? Which stages are stable or have a " \
+    "fluctuating duration? Is there a decrease or increase in average " \
+    "build duration over time?\n" \
+    "With these insights you can improve the stability of your build " \
+    "process and make it more efficient.\n\n" \
+    "The generation of timing data is done with either a client or using " \
+    "Buildtime Trend as a Service.\n"
+    "The Python based client generates custom timing tags for any shell " \
+    "based build process and can easily be integrated. A script processes " \
+    "the generated timing tags when the build is finished, and stores " \
+    "the results.\n" \
+    "Buildtime Trend as a Service gets timing and build related data by " \
+    "parsing the logfiles of a buildprocess. Currently, Travis CI is " \
+    "supported. Simply trigger the service at the end of a Travis CI build " \
+    "and the parsing, aggregating and storing of" \
+    "the data is done automatically.",
     url="https://buildtimetrend.github.io/",
     license="AGPLv3+",
     keywords=["trends", "charts", "build", "ci", "timing data"],
