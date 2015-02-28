1861465da2b5d7e524e73817fd67ad45b53f766b
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-03-01 00:25:12 +0100

    improve tests for process_build_logs as a generator

--- buildtimetrend/__init__.py
+++ buildtimetrend/__init__.py
@@ -29,7 +29,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import logging
 
 NAME = "buildtimetrend"
-VERSION = "0.3.dev3"
+VERSION = "0.3.dev4"
 SCHEMA_VERSION = "2"
 USER_AGENT = "%s/%s" % (NAME, VERSION)
 
