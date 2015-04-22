0ac1006b1755182d983de45cf6f558e86fe14775
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-04-22 21:19:15 +0200

    increment build number

--- buildtimetrend/__init__.py
+++ buildtimetrend/__init__.py
@@ -29,7 +29,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import logging
 
 NAME = "buildtimetrend"
-VERSION = "0.3.dev6"
+VERSION = "0.3.dev7"
 SCHEMA_VERSION = "3"
 USER_AGENT = "%s/%s" % (NAME, VERSION)
 
