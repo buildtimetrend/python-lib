5b462132ee0bbed5bd30efef9d2107306fe59ee5
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-06-03 21:10:55 +0200

    move is_repo_allowed() and format_duration() from service to library

---
+++ buildtimetrend/service.py
@@ -0,0 +1,85 @@
+# vim: set expandtab sw=4 ts=4:
+"""
+Service related methods.
+
+Copyright (C) 2014-2015 Dieter Adriaenssens <ruleant@users.sourceforge.net>
+
+This file is part of buildtimetrend/python-lib
+<https://github.com/buildtimetrend/python-lib/>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from buildtimetrend import logger
+from buildtimetrend.settings import Settings
+
+
+def is_repo_allowed(repo):
+    """
+    Check if repo is allowed.
+
+    A repository name is checked against a list of denied and allowed repos.
+    The 'denied_repo' check takes precendence over 'allowed_repo' check.
+    The list of denied/allowed repos is defined with settings 'denied_repo'
+    and 'allowed_repo'.
+    If the settings are not defined,
+    the repo is not checked against the denied/allowed lists.
+    Both 'denied_repo' and 'allowed_repo' can have multiple values,
+    if any of them matches a substring of the repo, the repo is denied/allowed.
+
+    Parameters:
+    -repo : repository name
+    """
+    if repo is None:
+        logger.warning("Repo is not defined")
+        return False
+
+    denied_message = "Project '%s' is not allowed."
+    denied_repo = Settings().get_setting("denied_repo")
+    allowed_repo = Settings().get_setting("allowed_repo")
+
+    if denied_repo is not None and \
+            any(x in repo for x in denied_repo) or \
+            allowed_repo is not None and \
+            not any(x in repo for x in allowed_repo):
+        logger.warning(denied_message, repo)
+        return False
+
+    return True
+
+
+def format_duration(duration):
+    """
+    Format duration from seconds to hours, minutes and seconds.
+
+    Parameters:
+    - duration : duration in seconds
+    """
+    if type(duration) not in (float, int) or duration < 0:
+        return "unknown"
+
+    seconds = duration % 60
+    duration = duration / 60
+    format_string = "{:.1f}s".format(seconds)
+
+    if duration >= 1:
+        minutes = int(duration % 60)
+        duration = duration / 60
+        format_string = "{:d}m {:s}".format(minutes, format_string)
+
+        if duration >= 1:
+            hours = int(duration % 60)
+            format_string = "{:d}h {:s}".format(hours, format_string)
+
+    return format_string
---
+++ buildtimetrend/test/service_test.py
@@ -0,0 +1,117 @@
+# vim: set expandtab sw=4 ts=4:
+"""
+Unit tests for Service
+
+Copyright (C) 2014-2015 Dieter Adriaenssens <ruleant@users.sourceforge.net>
+
+This file is part of buildtimetrend/service
+<https://github.com/buildtimetrend/service/>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from buildtimetrend.service import is_repo_allowed
+from buildtimetrend.service import format_duration
+from buildtimetrend.settings import Settings
+import unittest
+
+
+class TestService(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.settings = Settings()
+
+    def setUp(self):
+        # reinit settings singleton
+        if self.settings is not None:
+            self.settings.__init__()
+
+    def test_is_repo_allowed(self):
+        # error is thrown when called without parameters
+        self.assertRaises(TypeError, is_repo_allowed)
+
+        # error is thrown when called with an invalid parameter
+        self.assertFalse(is_repo_allowed(None))
+
+        # repo is allowed by default
+        self.assertTrue(is_repo_allowed("name/repo"))
+
+    def test_is_repo_allowed_set_denied(self):
+        # test denied repo
+        self.settings.add_setting("denied_repo", {"test1"})
+
+        self.assertTrue(is_repo_allowed("name/repo"))
+        self.assertFalse(is_repo_allowed("name/test1"))
+        self.assertTrue(is_repo_allowed("name/test2"))
+
+    def test_is_repo_allowed_set_denied_multi(self):
+        # test multiple denied repos
+        self.settings.add_setting("denied_repo", {"test1", "test2"})
+
+        self.assertTrue(is_repo_allowed("name/repo"))
+        self.assertFalse(is_repo_allowed("name/test1"))
+        self.assertFalse(is_repo_allowed("name/test2"))
+
+    def test_is_repo_allowed_set_allowed(self):
+        # test allowed repo
+        self.settings.add_setting("allowed_repo", {"test1"})
+
+        self.assertFalse(is_repo_allowed("name/repo"))
+        self.assertTrue(is_repo_allowed("name/test1"))
+        self.assertFalse(is_repo_allowed("name/test2"))
+
+    def test_is_repo_allowed_set_allowed_multi(self):
+        # test multiple allowed repos
+        self.settings.add_setting("allowed_repo", {"test1", "test2"})
+
+        self.assertFalse(is_repo_allowed("name/repo"))
+        self.assertTrue(is_repo_allowed("name/test1"))
+        self.assertTrue(is_repo_allowed("name/test2"))
+
+    def test_is_repo_allowed_set_denied_allowed(self):
+        # set denied repo
+        self.settings.add_setting("denied_repo", {"test1"})
+        # set allowed repo
+        self.settings.add_setting("allowed_repo", {"name"})
+
+        self.assertTrue(is_repo_allowed("name/repo"))
+        self.assertFalse(is_repo_allowed("name/test1"))
+        self.assertTrue(is_repo_allowed("name/test2"))
+        self.assertFalse(is_repo_allowed("owner/repo"))
+
+    def test_format_duration(self):
+        # error is thrown when called without parameters
+        self.assertRaises(TypeError, format_duration)
+
+        self.assertEquals("unknown", format_duration(None))
+        self.assertEquals("unknown", format_duration("string"))
+        self.assertEquals("unknown", format_duration(-1))
+
+        self.assertEquals("0.0s", format_duration(0))
+        self.assertEquals("1.0s", format_duration(1))
+        self.assertEquals("1.6s", format_duration(1.6))
+        self.assertEquals("1.6s", format_duration(1.63))
+        self.assertEquals("1.7s", format_duration(1.67))
+
+        self.assertEquals("59.7s", format_duration(59.7))
+        self.assertEquals("1m 0.0s", format_duration(60))
+        self.assertEquals("1m 0.3s", format_duration(60.3))
+        self.assertEquals("1m 1.0s", format_duration(61))
+
+        self.assertEquals("59m 59.7s", format_duration(3599.7))
+        self.assertEquals("1h 0m 0.0s", format_duration(3600))
+        self.assertEquals("1h 0m 0.3s", format_duration(3600.3))
+        self.assertEquals("1h 0m 1.0s", format_duration(3601))
+
+        self.assertEquals("2h 5m 0.0s", format_duration(7500))
