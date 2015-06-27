a6f33d55c3710a61f0a355a7face9bf4c85f839f
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-06-27 20:09:08 +0200

    use Keen.io query caching to speed up badge generation, issue #69

--- buildtimetrend/__init__.py
+++ buildtimetrend/__init__.py
@@ -29,7 +29,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import logging
 
 NAME = "buildtimetrend"
-VERSION = "0.3.dev14"
+VERSION = "0.3.dev15"
 SCHEMA_VERSION = "3"
 USER_AGENT = "%s/%s" % (NAME, VERSION)
 
