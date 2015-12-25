552f6cad6534dd7226a291664e4056f460840b57
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-12-25 20:40:48 +0100

    avoid using wildcard import

--- buildtimetrend/test/settings_test.py
+++ buildtimetrend/test/settings_test.py
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from buildtimetrend.settings import *
+from buildtimetrend.settings import Settings
 from buildtimetrend import logger
 from buildtimetrend import set_loglevel
 from buildtimetrend.collection import Collection
